e38b66f66c94288e927d01d47a5e274822ad1b71
Mayu Ruiz <mayu.ruiz@lambdaloopers.com>
2022-12-21 13:44:06 +0100

    refactor

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -5,14 +5,28 @@ export const fizzbuzz = (number) => {
 
     if(typeof number != 'number') throw new Error('param must be a number')  
     if(Number.isNaN(number)) throw new Error('param must be a number')
+    
     //return 1
 
     //if(number%5 == 0 && number%3 == 0) return 'fizzbuzz'
-    if(number%15 == 0) return 'fizzbuzz'
-    if(number%3 == 0) return 'fizz'
-    if(number%5 == 0) return 'buzz'
-    return number
-}
+    // if(number%15 == 0) return 'fizzbuzz'
+    // if(number%3 == 0) return 'fizz'
+    // if(number%5 == 0) return 'buzz'
+    // return number
 
 
 // REFACTOR:
+
+const multiples = { 3: 'fizz', 5: 'buzz' }
+let output = ''
+
+Object.entries(multiples).forEach(([multiplier, word]) =>{
+    if(number % multiplier == 0) output += word
+})
+
+return output == '' ? number : output
+
+}
+
+
+
